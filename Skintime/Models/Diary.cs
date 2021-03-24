493ea7573e562d871f8772e9a39b763612d4a321
pbpbpb2705 <78841037+pbpbpb2705@users.noreply.github.com>
2021-03-24 09:42:02 +0700

    Update Diary.cs

--- Skintime/Models/Diary.cs
+++ Skintime/Models/Diary.cs
@@ -7,7 +7,7 @@ namespace Skintime.Models
     public class Diary
     {
         [PrimaryKey, AutoIncrement,Indexed]
-        public int ID { get; set; }
+        public int? ID { get; set; }
         public DateTime Date { get; set; }
         public string Text { get; set; }
     }
