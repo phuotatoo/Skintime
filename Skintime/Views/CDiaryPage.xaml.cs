ef7053616633f3c2603fba951a45ef8a13306ce9
phuotatoo <pphuonganhwork@gmail.com>
2021-04-08 20:40:36 +0700

    chettoimat

--- Skintime/Views/CDiaryPage.xaml.cs
+++ Skintime/Views/CDiaryPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Skintime.Models;
+using Xamarin.Plugin.Calendar.Models;
 
 namespace Skintime.Views
 {
@@ -18,10 +19,39 @@ namespace Skintime.Views
             InitializeComponent();
         }
 
+        public EventCollection Events { get; set; }
+
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            List<Diary> items = await App.Database.GetDiaryAsync();
+            Events = new EventCollection
+            {
+                /*
+                [DateTime.Now] = new List<Diary>
+                {
+                    new Diary { Name = "Cool event1", Description = "This is Cool event1's description!" }
+                },
+
+                
+                // 5 days from today
+                [DateTime.Now.AddDays(5)] = new List<Diary>
+                {
+                    new Diary { Name = "Cool event3", Description = "This is Cool event3's description!" }
+                },
+
+                // 3 days ago
+                [DateTime.Now.AddDays(-3)] = new List<Diary>
+                {
+                    new Diary { Name = "Cool event5", Description = "This is Cool event5's description!" }
+                },
+
+                // custom date
+                [new DateTime(2020, 3, 16))] = new List<Diary>
+                {
+                    new Diary { Name = "Cool event6", Description = "This is Cool event6's description!" }
+                }
+                */
+            };
 
         }
     }
